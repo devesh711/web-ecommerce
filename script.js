59906e37c646d2f71ec6d071ea20533f996dad22
devesh711 <pdevesh699@gmail.com>
2024-01-07 15:16:16 +0530

    renaming img folder

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+const Quantity = document.getElementsByClassName("quantity");
+const subTotal = document.getElementsByClassName("subtotal");
+const price = document.getElementsByClassName("price");
+const CartTotal = document.getElementsByClassName("cartTotal");
+function inputCheck() {
+  for (let x = 0; x < Quantity.length; x++) {
+    subTotal[x].innerHTML = Quantity[x].value * price[x].innerHTML;
+    CartTotal[1].innerHTML += subTotal[x].innerHTML;
+    CartTotal[0].innerHTML += subTotal[x].innerHTML;
+  }
+  // or do something else
+}
+
+for (let x = 0; x < Quantity.length; x++) {
+  Quantity[x].addEventListener("change", inputCheck);
 }
